74f5555de07d9a826cf130db0779f5c22b83aabb
Justin <juyoung101@msn.com>
2023-10-31 22:29:37 -0600

    Update discordRpcProvider.js
    how did i get so many commas omg

--- src/providers/discordRpcProvider.js
+++ src/providers/discordRpcProvider.js
@@ -1,86 +1,82 @@
-const clientId = '495666957501071390'
-const RPC = require('discord-rpc')
-const settingsProvider = require('./settingsProvider')
+const clientId = '495666957501071390';
+const RPC = require('discord-rpc');
+const settingsProvider = require('./settingsProvider');
 
-let client
-let _isStarted
+let client;
+let _isStarted;
 
 function isStarted() {
-    return _isStarted
+    return _isStarted;
 }
 
 function _setIsStarted(value) {
-    _isStarted = value
+    _isStarted = value;
 }
 
 function start() {
-    client = new RPC.Client({ transport: 'ipc' })
+    client = new RPC.Client({ transport: 'ipc' });
 
-    client.on('ready', () => _setIsStarted(true))
+    client.on('ready', () => _setIsStarted(true));
 
     client.login({ clientId }).catch(() => {
         if (!isStarted()) {
-            setTimeout(() => {
-                start()
-            }, 10000)
+            setTimeout(() => {start();}, 10000);
         }
-    })
+    });
 
     client.on('disconnected', () => {
-        _setIsStarted(false)
-        start()
-    })
+        _setIsStarted(false);
+        start();
+    });
 }
 
 function stop() {
-    client.destroy()
-    _setIsStarted(false)
+    client.destroy();
+    _setIsStarted(false);
 }
 
 async function setActivity(info) {
     if (isStarted() && info.track.title) {
-        const now = Date.now()
-        const activity = {}
-        const discordSettings = settingsProvider.get('discord-presence-settings')
+        const now = Date.now();
+        const activity = {};
+        const discordSettings = settingsProvider.get('discord-presence-settings');
 
-        if (discordSettings.details) activity.details = info.track.title
+        if (discordSettings.details){
+            activity.details = info.track.title;
+        }
 
-        if (discordSettings.state) activity.state = info.track.author
+        if (discordSettings.state) {
+            activity.state = info.track.author;
+        }
 
         if (discordSettings.time) {
             if (info.player.isPaused) {
-                delete activity.startTimestamp
-                delete activity.endTimestamp
+                delete activity.startTimestamp;
+                delete activity.endTimestamp;
             } else {
-                activity.startTimestamp =
-                    now + info.player.seekbarCurrentPosition * 1000
-                activity.endTimestamp =
-                    now +
-                    (info.track.duration - info.player.seekbarCurrentPosition) *
-                        1000
+                activity.startTimestamp = now + info.player.seekbarCurrentPosition * 1000;
+                activity.endTimestamp = now + (info.track.duration - info.player.seekbarCurrentPosition) * 1000;
             }
         }
 
         // activity.largeImageKey = 'ytm_logo_512'
-        activity.type = 2 // (2) "Listening to {name}" instead of (1) "Playing {name}"
-        activity.largeImageKey = info.track.cover
-        activity.smallImageKey = info.player.isPaused
-            ? 'discordrpc-pause'
-            : 'discordrpc-play'
-        activity.largeImageText = 'YouTube Music'
-        activity.smallImageText = info.player.isPaused ? 'Paused' : 'Playing'
-        activity.instance = false
+        activity.type = 2; // (2) "Listening to {name}" instead of (1) "Playing {name}"
+        activity.largeImageKey = info.track.cover;
+        activity.smallImageKey = info.player.isPaused ? 'discordrpc-pause' : 'discordrpc-play';
+        activity.largeImageText = 'YouTube Music';
+        activity.smallImageText = info.player.isPaused ? 'Paused' : 'Playing';
+        activity.instance = false;
         if (discordSettings.details) {
             activity.buttons = [
                 {
                     label: 'Play on YouTube Music',
                     url: 'https://music.youtube.com/watch?v=' + info.track.id
                 }
-            ]
+            ];
         }
 
         if ((!discordSettings.hideIdle && info.player.isPaused) || info.track.isAdvertisement) {
-            await client.clearActivity()
+            await client.clearActivity();
         } else {
             // As of writing this discord-rpc was not updated to support buttons with setActivity
             await client.request('SET_ACTIVITY', {
@@ -91,18 +87,18 @@ async function setActivity(info) {
                     type: activity.type,
                     timestamps: {
                         start: activity.startTimestamp,
-                        end: activity.endTimestamp,
+                        end: activity.endTimestamp
                     },
                     assets: {
                         large_image: activity.largeImageKey,
                         large_text: activity.largeImageText,
                         small_image: activity.smallImageKey,
-                        small_text: activity.smallImageText,
+                        small_text: activity.smallImageText
                     },
                     instance: activity.instance,
-                    buttons: activity.buttons,
+                    buttons: activity.buttons
                 }
-            })
+            });
         }
     }
 }
@@ -111,5 +107,5 @@ module.exports = {
     isStarted: isStarted,
     start: start,
     stop: stop,
-    setActivity: setActivity,
-}
+    setActivity: setActivity
+};
